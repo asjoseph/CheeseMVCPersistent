89b6ea225ae69dea2f01e91d4432e751e6087ed5
asjoseph <34357308+asjoseph@users.noreply.github.com>
2018-04-01 23:56:39 -0400

    Add files via upload
    Updated

---
+++ ViewModels/AddCheeseViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCheeseViewModel
+    {
+        [Required]
+        [Display(Name = "Cheese Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You must give your cheese a description")]
+        public string Description { get; set; }
+
+        //public CheeseType Type { get; set; }
+
+        //public List<SelectListItem> CheeseTypes { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryID { get; set; }
+
+        public List<SelectListItem> Categories { get; set; }
+        public AddCheeseViewModel() { }
+        public AddCheeseViewModel(IEnumerable<CheeseCategory> categories)
+        {
+            Categories = new List<SelectListItem>();
+
+            foreach (var category in categories)
+            {
+                // <option value="0">Hard</option>
+                Categories.Add(new SelectListItem
+                {
+                    Value = category.ID.ToString(),
+                    Text = category.Name.ToString()
+                });
+
+                //CheeseTypes.Add(new SelectListItem
+                //{
+                //Value = ((int)CheeseType.Soft).ToString(),
+                //Text = CheeseType.Soft.ToString()
+                //});
+
+                //CheeseTypes.Add(new SelectListItem
+                //{
+                //Value = ((int)CheeseType.Fake).ToString(),
+                //Text = CheeseType.Fake.ToString()
+                //});
+            }
+        }
+    }
+}
